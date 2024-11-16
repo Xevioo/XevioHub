4894753b8ccdeb2ed02b59908e5a8e0bfa8763b8
Xevioo <147429168+Xevioo@users.noreply.github.com>
2024-11-16 05:54:22 -0500

    Update CritScript.bat

--- CritScript.bat
+++ CritScript.bat
@@ -2,9 +2,12 @@
 
 :: BatchGotAdmin
 :-------------------------------------
+REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
+REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
     goto UACPrompt
 ) else ( goto gotAdmin )
 
