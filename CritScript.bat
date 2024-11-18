bd8e65204dd7f3510bc9d8e42a91aaa7de78afc3
Xevioo <147429168+Xevioo@users.noreply.github.com>
2024-11-17 19:47:37 -0500

    Update CritScript.bat

--- CritScript.bat
+++ CritScript.bat
@@ -80,12 +80,10 @@ echo Using desktop path: %desktopPath%
 :CreateShortcut
 :: Step 6: Create a shortcut for ZOMBIES.AHK on the determined desktop path
 set "zombiesScript=%TEMP%\ZOMBIES.AHK"
-set "iconPath=%TEMP%\icon.ico"
 set "iconPath=%TEMP%\ahk.ico"
 
 if exist "%zombiesScript%" (
     echo Creating shortcut for ZOMBIES.AHK on the desktop...
-    Powershell -ExecutionPolicy Bypass -File "%TEMP%\CreateShortcut.ps1"
     Powershell -ExecutionPolicy Bypass -File "%TEMP%\shortcut.ps1"
     echo Shortcut created.
 ) else (
